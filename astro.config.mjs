79be091db0cef7150fc725eef22978f68dbe7e80
FSM <joseaugsto.teo@gmail.com>
2024-01-05 21:23:46 -0300

    Conclusão da página inicial e melhorias na estruturação das páginas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [
-    tailwind(),
-  ],
+  integrations: [tailwind()],
   i18n: {
     defaultLocale: "pt-br",
     locales: ["en", "es", "pt-br"],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+  adapter: vercel()
 });
