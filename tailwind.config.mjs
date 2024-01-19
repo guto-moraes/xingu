a301f9f4fe5b99d4c6c93dd664dbdcfe9a834298
FSM <joseaugsto.teo@gmail.com>
2024-01-19 02:10:14 -0300

    Correção do carregamento da imagem de fundo da página inicial

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			backgroundImage: {
-				roncador: 'url("./assets/images/serra-do-roncador.jpg")'
-			},
 			gridTemplateRows: {
 				layout: "80px auto 48px",
 			}
