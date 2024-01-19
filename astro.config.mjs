8db6a06e2e951176f7f4c5e88f56ced67431c934
FSM <joseaugsto.teo@gmail.com>
2024-01-19 00:46:18 -0300

    Atuação do Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "pt-br",
